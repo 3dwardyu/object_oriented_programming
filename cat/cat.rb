2ea5a792c6cea86960935dcf388150b8535750a9
Edward Yu <3dward.yu@gmail.com>
2016-08-10 16:17:59 -0400

    correct instance variables and add eats method

--- cat/cat.rb
+++ cat/cat.rb
@@ -1,10 +1,19 @@
 class Cat
+  attr_accessor :name
+  attr_accessor :preferred_food
+  attr_accessor :meal_time
   def initialize(name, preferred_food, meal_time)
-    attr_accessor :name
-    attr_accessor :preferred_food
-    attr_accessor :meal_time
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time =  meal_time
+  end
+  def eats_at
+    puts Time.at(@meal_time).strftime("%l %p")
+
   end
 end
 
 rengar = Cat.new("Rengar", "dry foods", 15)
 teemo = Cat.new("Teemo", "mushrooms", 23)
+
+rengar.eats_at
