58decb8f6fdb7c26e35af9ef047d0cd335f63a51
Edward Yu <3dward.yu@gmail.com>
2016-08-11 12:07:10 -0400

    create variables for paperboy

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,8 +1,16 @@
 class Paperboy
+  # name, exp, side have read and write access
+  # earnings is read only
   attr_accessor :name
   attr_accessor :exp
   attr_accessor :side
   attr_reader :earnings
+
+  # initialize paperboy with 4 variables
   def initialize(name, exp, side, earnings)
+    @name = name
+    @exp = exp
+    @side = side
+    @earnings = earnings
 end
 end
