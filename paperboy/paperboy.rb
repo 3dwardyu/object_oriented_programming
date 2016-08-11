c2ff11ca3481394879d2287cfdb8e83519aab30b
Edward Yu <3dward.yu@gmail.com>
2016-08-11 14:58:37 -0400

    add deliver method

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -17,10 +17,19 @@ class Paperboy
   end
 
   def deliver(street_start, street_end)
-
+    total_delv = (street_end.to_f - street_start)/2
+    total_earned = 0
+    if total_delv <= 50
+      total_earned = total_delv * 0.25
+      return total_earned
+    else
+      total_earned = (50 * 0.25) + ((total_delv - 50) * 0.50)
+      return total_earned
   end
+end
 
   def report
+    
   end
 
 end
