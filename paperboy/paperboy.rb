ded2d5cd671c49a50dfb9bcb58300cb6916ef2eb
Edward Yu <3dward.yu@gmail.com>
2016-08-11 17:29:19 -0400

    edit deliver method

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -10,26 +10,54 @@ class Paperboy
     @exp = exp
     @side = side
     @earnings = earnings
+    @quota = exp
   end
 
   def quota
-    q = 50 + @exp/2
+    @quota = 50 + @exp/2
+    return @quota
   end
 
   def deliver(street_start, street_end)
-    total_delv = (street_end.to_f - street_start)/2
-    total_earned = 0
-    if total_delv <= 50
-      total_earned = total_delv * 0.25
-      return total_earned
+    houses = []
+    earn = 0
+    ex = 0
+    # finds out how many houses are being delived to
+  if @side == "even"
+    houses = (street_start..street_end).find_all { |n| n % 2 == 0 }
+    exp = houses.length
+    if @exp <= @quota
+      earn = ex * 0.25
     else
-      total_earned = (50 * 0.25) + ((total_delv - 50) * 0.50)
-      return total_earned
+      earn = (@quota + 0.25) + ((ex - @quota) * 0.50)
+    end
+
+  elsif side == "odd"
+    houses = (street_start..street_end).find_all { |n| n % 2 != 0 }
+    ex = houses.length
+    if @exp <= @quota
+      earn = ex * 0.25
+    else
+      earn += (@quota + 0.25) + ((ex - @quota) * 0.50)
+    end
+
+  else
+    earn -= 2
   end
+ @earnings += earn
+ @exp += ex
 end
+
 
   def report
-    
+    puts "I'm #{@name}, I've delivered #{@exp} papers and I've earned #{@earnings} so far!"
   end
 
 end
+
+tommy = Paperboy.new("Tommy", 0, "even", 0)
+
+tommy.quota
+tommy.deliver(101, 220)
+tommy.earnings
+tommy.report
