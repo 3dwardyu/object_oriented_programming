3074404d16dc72b18adbc082a216ec44e04557e4
Edward Yu <3dward.yu@gmail.com>
2016-08-12 01:09:23 -0400

    refined instructions method

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -8,32 +8,36 @@ class Rover
   end
 
   def read_instructions(command)
-  instruct = command.split
-    for instruct.each do |letter|
+
+  instruct = command.split(",")
+    instruct.each do |letter|
+
       # if the array contains "M" it'll run move method and move one position
-      if letter = "M"
-      self.move(letter)
+      if letter == "M"
+        self.move(letter)
       # if the array contains "R" or "L" it'll run turn method with the correct direction
-      elsif letter = "R" || "L"
-      self.turn(letter)
+    elsif letter == "R" || letter == "L"
+        self.turn(letter)
       #if it doesn't read letters other than "M, R or L" it will assume you are giving cordinates for the rover
       else
-      @x = instruct[0]
-      @y = instruct[1]
-      @d = instruct[2]
-    end
-  end
+        @x = instruct[0]
+        @y = instruct[1]
+        @d = instruct[2]
+      end
 
+    end
+    puts "#{@x} #{@y} #{@d}"
+end
 
   def move(d)
   # determines where on the grid the rover will move
     case d
-    when @d = "N" then y + 1
-    when @d = "E" then x + 1
-    when @d = "S" then y - 1
-    else x - 1
+    when @d = "N" then @y + 1
+    when @d = "E" then @x + 1
+    when @d = "S" then @y - 1
+    else @x - 1
     end
-    return @x, @y
+
   end
 
   def turn(t)
@@ -54,10 +58,16 @@ class Rover
       when "S" then @d = "E"
       else @d = "N"
       end
+
     end
-    return @d
+
   end
 end
 
+
+
 size = gets
 doge = Rover.new(gets, gets, gets)
+doge.read_instructions(gets)
+walle = Rover.new(gets, gets, gets)
+walle.read_instructions(gets)
